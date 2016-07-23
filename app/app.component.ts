3fd50d215c99dad2ccc5804a1f4a02f198b5febc
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-07-23 16:45:44 +0100

    add simple counter example based on @ngrx/store

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,33 @@
 import {Component, OnInit} from '@angular/core';
+import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
+import {HTTP_PROVIDERS} from '@angular/http';
+import {StoreLogMonitorComponent} from '@ngrx/store-log-monitor';
+
+import {CounterComponent} from './counter/counter.component';
+
+// Providers
+import {STORE_PROVIDERS} from './core/states';
 
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  directives: [
+    CounterComponent,
+    StoreLogMonitorComponent,
+  ],
+  providers: [
+    ROUTER_PROVIDERS, HTTP_PROVIDERS,
+    STORE_PROVIDERS,
+  ]
 })
 export class AppComponent implements OnInit {
-  public title: string;
+
+  title: string;
+
+  constructor() { }
 
   ngOnInit() {
     this.title = 'Hello world!';
   }
 }
-
