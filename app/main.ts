ee4cc8280c3361f1709d0047c0b0684cfa187e73
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-07-31 23:54:47 +0100

    turn off deprecated forms

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,9 @@
-import { AppComponent } from './app.component';
-import { bootstrap } from '@angular/platform-browser-dynamic';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {disableDeprecatedForms, provideForms} from '@angular/forms';
+import {AppComponent} from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+    disableDeprecatedForms(),
+    provideForms(),
+]);
 
