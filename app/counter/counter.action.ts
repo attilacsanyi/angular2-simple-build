9985990cd440b622d7f7c2bfbe79f211f7acdc91
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-08-06 11:10:41 +0100

    use better action types

--- app/counter/counter.action.ts
+++ app/counter/counter.action.ts
@@ -1,22 +1,22 @@
 import {BaseAction} from '../core/models';
 
 // Counter Actions
-const PREFIX = 'COUNTER_';
+const PREFIX = '[COUNTER]';
 
 export class IncrementCounterAction extends BaseAction {
   constructor(public amount: number) {
-    super(`${PREFIX}INCREMENT`);
+    super(`${PREFIX} Increment`);
   }
 }
 
 export class DecrementCounterAction extends BaseAction {
   constructor(public amount: number) {
-    super(`${PREFIX}DECREMENT`);
+    super(`${PREFIX} Decrement`);
   }
 }
 
 export class ResetCounterAction extends BaseAction {
   constructor() {
-    super(`${PREFIX}RESET`);
+    super(`${PREFIX} Reset`);
   }
 }
