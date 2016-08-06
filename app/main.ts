77320fff68c0d730cf1e854ae47ed9d323a1b7f6
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-08-06 11:25:03 +0100

    add enable prod mode for angular2

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,12 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
 import {disableDeprecatedForms, provideForms} from '@angular/forms';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+// import {enableProdMode} from '@angular/core';
+
 import {AppComponent} from './app.component';
 import {appRouterProviders} from './app.routes';
+
+// Enable PROD mode
+// enableProdMode();
 
 bootstrap(AppComponent, [
     appRouterProviders,
